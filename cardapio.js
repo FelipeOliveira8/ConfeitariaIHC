dd94c90f30aeb483a7a1db7a2f5227e47ba4fac5
Felipe Oliveira <fc0669493@gmail.com>
2025-12-05 21:30:30 -0300

    merge fetch

--- cardapio.js
+++ cardapio.js
@@ -1,29 +1,27 @@
-let itensCardapio = [    
-    {
-        nome : "Bolo de chocolate",
-        preco : "39,90",
-        img: "img/bolo_de_chocolate.jpg"
-    },
-    {
-        nome: "Bolo de morango",
-        preco: "45,90",        
-        img: "img/bolo_de_morango.jpg"
-    },
-    {
-        nome: "Bolo de cenoura",
-        preco: "19,90",    
-        img: "img/bolo_de_cenoura.jpg"
-    }
-]
+async function buscarEndpoint() {
+    await fetch('https://confeitaria-api-fg5n.onrender.com/cardapio', {
+        'headers': {"Accept": "application/json"}
+    })
+    .then(json => {
+        retorno = json.json();
+    })
 
-function carregarItensCardapio(itens){
+    return retorno;
+}
+
+async function carregarItensCardapio(){
+    const itens = await buscarEndpoint();
+
     document.getElementById("itens-cardapio").innerHTML = '';
     for(let i = 0; i < itens.length; i++){
         document.getElementById("itens-cardapio").innerHTML += `<div>
-                <img src="${itens[i].img}" alt="${itens[i].nome}">
-                <h3>${itens[i].nome} - R$${itens[i].preco}</h3>
+                <img src="${itens[i].foto}" style="height: 100%!important;" alt="${itens[i].nome}">
+                <h3>${itens[i].nome} - R$${((itens[i].preco).toString()).replace('.', ',')}</h3>
             </div>`;
     }
+
+    console.log();
 }
 
-carregarItensCardapio(itensCardapio);
+
+carregarItensCardapio();
